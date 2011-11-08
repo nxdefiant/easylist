f48b4d1bb89d63188c74641a980b1836d583a185
Willem Liu <willem.liu@gmail.com>
2011-11-08 20:41:21 +0100

    Merged branch from Adekker. Added Thanks to Ade in About box.

--- mainform.cpp
+++ mainform.cpp
@@ -184,6 +184,7 @@ void MainForm::on_actionAbout_triggered()
     aboutText.append(QDate::currentDate().toString("yyyy"));
     aboutText.append("<br><br>");
     aboutText.append("Created by Willem Liu.<br>");
+    aboutText.append("Thanks to Ade.<br>");
     aboutText.append("Created with QtCreator.<br><br>");
     aboutText.append("Please <a href='http://www.willemliu.nl/donate'>donate</a> any amount you deem this app is worthy to keep me going on.<br><br>");
     aboutText.append("</body></html>");
